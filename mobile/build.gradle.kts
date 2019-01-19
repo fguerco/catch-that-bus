983d9384b3e85029a2ba11167ddd0bccb3b62e20
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2019-01-19 14:24:13 -0200

    Initial commit

---
+++ mobile/build.gradle.kts
@@ -0,0 +1,47 @@
+import com.android.sdklib.AndroidVersion
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        applicationId = "com.example.felipe.catchthatbus"
+        minSdkVersion(26)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(
+        mapOf("dir" to "libs",
+              "include" to listOf("*.jar"))
+    ))
+    implementation(kotlin("stdlib-jdk8", KotlinCompilerVersion.VERSION))
+    implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation("com.android.support:design:28.0.0")
+    implementation("com.android.support.constraint:constraint-layout:1.1.3")
+    implementation("com.android.support:support-annotations:28.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+}
+
+repositories {
+    mavenCentral()
+}
