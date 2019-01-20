fbd3d42a60e493948dc1d3465ae6e4abc54eddeb
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2019-01-20 05:08:36 -0200

    Temporarily changed main activity to linear layout

--- mobile/src/main/java/com/example/felipe/catchthatbus/DeparturesFragment.kt
+++ mobile/src/main/java/com/example/felipe/catchthatbus/DeparturesFragment.kt
@@ -7,25 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_departures.departures_list_view
 import kotlinx.android.synthetic.main.fragment_departures.view.departures_list_view
+import kotlinx.android.synthetic.main.fragment_departures.view.departures_pull_to_refresh
 
-/**
- * A placeholder fragment containing a simple view.
- */
 class DeparturesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_departures, container, false).apply {
             context?.let { departures_list_view.setAdapter(DeparturesListAdapter(it, departures_list_view))  }
-//            departures_pull_to_refresh.apply {
-//                setOnRefreshListener {
-//                    refresh()
-//                    isRefreshing = false
-//                }
-//            }
+            departures_pull_to_refresh.apply {
+                setOnRefreshListener {
+                    refresh()
+                    isRefreshing = false
+                }
+            }
         }
     }
 
     fun refresh() = (departures_list_view.expandableListAdapter as DeparturesListAdapter).refresh()
-
 }
